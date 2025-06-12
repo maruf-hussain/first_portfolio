368005683cb2b5de1365f40e7e1a31d4d0f9f915
maruf-hussain <engmarufhussain@gmail.com>
2025-06-12 23:49:58 +0600

    views setup complete

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Home Page......................
+def home(request):
+    return render(request,'home.html')
+
+#About Page.........................
+def about(request):
+    return render(request,'about.html')
+
+#Projects Page........................
+def projects(request):
+    return render(request,'projects.html')
+
+#Testimonial Page.....................
+def testimonial(request):
+    return render(request,'testimonial.html')
+
+#Hire Page............................
+def testimonial(request):
+    return render(request,'testimonial.html')
